0bbb23e02cc161005e0962850874b937a0856f7c
rok_1 <119943038+Roks2002@users.noreply.github.com>
2024-08-23 16:02:51 +0200

    corrected Rain gradient

--- weather-app/src/components/WeatherApp.jsx
+++ weather-app/src/components/WeatherApp.jsx
@@ -67,7 +67,6 @@ const WeatherApp = () => {
   const backgroundImages = {
     Clear: "linear-gradient(to right, #f3b07c, #fcd283)",
     Clouds: "linear-gradient(to right, #57d6d4, #71eeec)",
-    Rain: "linear-gradient(to right, #5bc8fb, #80eaff)",
     Rain: "linear-gradient(to right, #aff2ff, #fff)",
     Haze: "linear-gradient(to right, #57d6d4, #71eeec)",
     Mist: "linear-gradient(to right, #57d6d4, #71eeec)",
